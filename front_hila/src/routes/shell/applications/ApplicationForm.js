d99166e863f0c9f109ddb03876e682c3ac240505
hilaavr <hilaavr@post.bgu.ac.il>
2021-06-17 00:04:44 +0300

    limit the number of characters in new application

--- front_hila/src/routes/shell/applications/ApplicationForm.js
+++ front_hila/src/routes/shell/applications/ApplicationForm.js
@@ -1,12 +1,12 @@
-import {Form, Input, Upload, message, InputNumber, Button, ConfigProvider, Select, Space} from 'antd';
-import React, {useEffect, Component, useState} from 'react';
-import {sendApplication, addParticipantToEvent} from "../../../services/api-civicrm-service";
+import { Form, Input, Upload, message, InputNumber, Button, ConfigProvider, Select, Space } from 'antd';
+import React, { useEffect, Component, useState } from 'react';
+import { sendApplication, addParticipantToEvent } from "../../../services/api-civicrm-service";
 import applicationTypes from '../../../utils/application-type-translator';
 import "./Application-form.scss"
 
-const {Option} = Select;
+const { Option } = Select;
 
-const handleApplication = async ({title, summary, description}, userSession) => {
+const handleApplication = async ({ title, summary, description }, userSession) => {
     let current_time = new Date().toISOString()
     let _date = current_time.slice(0, 10)
     let _hour = current_time.slice(11, 19)
@@ -23,7 +23,7 @@ const handleApplication = async ({title, summary, description}, userSession) => 
     console.log(details.event_title)
     const sendRes = await sendApplication(details.api_key, details.event_title, details.event_type, details.event_description, summary, _date + " " + _hour);
     let keys = Object.keys(sendRes.data.values)
-    console.log("keys[0]",keys)
+    console.log("keys[0]", keys)
     const addParRes = await addParticipantToEvent(userSession.Data?.API_KEY, keys[0], userSession.Data.contact.contact_id)
     console.log("addParRes: ", addParRes)
 }
@@ -39,30 +39,30 @@ const ApplicationForm = props => {
             <Form onFinish={onFinish}>
                 <h3>נושא הפנייה:</h3>
                 <Form.Item
-                    name="title" className="login-from input" rules={[{required: false, message: 'יש לבחור סוג פניה'}]}>
-                    <select class = "selector" id = "app_subject" placeholder="נושא" allowClear required>
-                            <option selected="selected" disabled="disabled" value="">  אנא בחר את סוג הפנייה </option>
+                    name="title" className="login-from input" rules={[{ required: false, message: 'יש לבחור סוג פניה' }]}>
+                    <select class="selector" id="app_subject" placeholder="נושא" allowClear required>
+                        <option selected="selected" disabled="disabled" value="">  אנא בחר את סוג הפנייה </option>
                         {
                             Object.keys(applicationTypes).map(key =>
                                 <option key={key} value={key}>{applicationTypes[key]}</option>)
-                            
+
                         }
                     </select>
                 </Form.Item>
 
                 <h3>תקציר: </h3>
                 <Form.Item name="summary" className="login-from input">
-                    <Input/>
+                    <Input />
                 </Form.Item>
 
                 <h3>פירוט: </h3>
-                <Form.Item name="description">
+                <Form.Item name="description" maxLength="350">
                     <Input.TextArea placeholder=" פרט את בקשתך כאן (ישאר חסוי)" size={"flex"}
-                                    className={"descriptionn"}/>
+                        className={"descriptionn"} />
                 </Form.Item>
 
                 <Form.Item>
-                    <Button id = "send_app" type="primary" shape="round" htmlType="submit" className={"ant-btn-app"}>
+                    <Button id="send_app" type="primary" shape="round" htmlType="submit" className={"ant-btn-app"}>
                         שלח
                     </Button>
                 </Form.Item>
