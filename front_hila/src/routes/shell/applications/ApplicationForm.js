6db2ca274ccb21f82c7e4a4888f26b9fd63afcd3
Amitlevizky2 <levamit@post.bgu.ac.il>
2021-06-17 00:42:48 +0300

    fixed message template

--- front_hila/src/routes/shell/applications/ApplicationForm.js
+++ front_hila/src/routes/shell/applications/ApplicationForm.js
@@ -7,25 +7,34 @@ import "./Application-form.scss"
 const { Option } = Select;
 
 const handleApplication = async ({ title, summary, description }, userSession) => {
-    let current_time = new Date().toISOString()
-    let _date = current_time.slice(0, 10)
-    let _hour = current_time.slice(11, 19)
-    console.log(`THE DATE IS: ${new Date().toISOString()}`)
-    debugger;
-    const details = {
-        api_key: userSession.Data?.API_KEY,
-        event_title: title,
-        summary,
-        event_description: description,
-        event_type: title
-    };
+    try {
+        let current_time = new Date().toISOString()
+        let _date = current_time.slice(0, 10)
+        let _hour = current_time.slice(11, 19)
+        debugger;
+        const details = {
+            api_key: userSession.Data?.API_KEY,
+            event_title: title,
+            summary,
+            event_description: description,
+            event_type: title
+        };
 
-    console.log(details.event_title)
-    const sendRes = await sendApplication(details.api_key, details.event_title, details.event_type, details.event_description, summary, _date + " " + _hour);
-    let keys = Object.keys(sendRes.data.values)
-    console.log("keys[0]", keys)
-    const addParRes = await addParticipantToEvent(userSession.Data?.API_KEY, keys[0], userSession.Data.contact.contact_id)
-    console.log("addParRes: ", addParRes)
+        if (description.length > 350){
+            throw "פנייתך ארוכה מידי כדי להתקבל למערכת. פניה יכולה להיות בגודל של עד 350 תווים, אנה נסה שוב והקפד על אורך תקין"
+        }
+
+        console.log(details.event_title)
+        const sendRes = await sendApplication(details.api_key, details.event_title, details.event_type, details.event_description, summary, _date + " " + _hour);
+        let keys = Object.keys(sendRes.data.values)
+        console.log("keys[0]", keys)
+        const addParRes = await addParticipantToEvent(userSession.Data?.API_KEY, keys[0], userSession.Data.contact.contact_id)
+        console.log("addParRes: ", addParRes)
+
+        message.success("הפניה נוצרה בהצלחה!")
+    } catch (e) {
+        message.error(e)
+    }
 }
 
 const ApplicationForm = props => {
